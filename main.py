d897f983e3de5aa815ada14af15ddbc688819e29
cgmaiorano <113949028+cgmaiorano@users.noreply.github.com>
2024-05-29 15:10:58 -0400

    Update main.py
    updated so sessions and labels are arguments

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ def main():
     """This is the main function responsible for audio segment trimming and curation."""
     args = parse_arguments()
     data_run_folder_name = args.data_run_folder_name
+    sessions_path = args.sessions
+    labels_path = args.labels
     
-    audio_segments, sessions, data_run_path, labels = setup_directories("data", data_run_folder_name)
+    audio_segments, sessions, data_run_path, labels = setup_directories("data", data_run_folder_name, sessions_path, labels_path)
     sessions_list = [f for f in os.listdir(sessions) if f.endswith(".wav")]
     
     if not os.path.exists(data_run_path):
